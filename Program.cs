748bb2246b91e3fb49238caf69cd97c9110b4c2a
MuhDavin213 <davin.new58@gmail.com>
2022-10-24 16:11:20 +0700

    menambahkan method true false

--- Program.cs
+++ Program.cs
@@ -7,8 +7,20 @@ using System.Threading.Tasks;
 namespace SearchAlgorithm
 {
     internal class Program
-    {
-        int[] arr 
+    {   //array to be reached
+        int[] arr = new int[20];
+        //number of elements in the array
+        int n;
+        //get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+
+            }
+        }
         static void Main(string[] args)
         {
         }
